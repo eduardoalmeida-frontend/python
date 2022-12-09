12b9c32325ab1e217b1eb69695a21c35f3e43bf8
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2022-12-09 07:37:56 -0300

    início da resolução do ex080

--- Mundo 3 - Estruturas Compostas/ex080.py
+++ Mundo 3 - Estruturas Compostas/ex080.py
@@ -1,3 +1,22 @@
 # Crie um programa onde o usuário possa digitar cinco valores númericos.
 # Cadastre-os em uma lista, já na posição correta de inserção (sem usasr o sort()).
 # No final, mostre a lista ordenada na tela.
+lista = []
+
+for i in range(0, 5):
+    if len(lista) == 0:
+        lista.append(int(input('Digite um valor: ')))
+        print('Adicionado ao final da lista...')
+    else:
+        num = int(input('Digite um valor: '))
+        for i in range(0, len(lista)):
+            if len(lista) == 1:
+                if max(lista) > num:
+                    lista.insert(0, num)
+                    print('Adicionado na posição 0 da lista...')
+            if min(lista) > num:
+                lista.insert(0, num)
+                print('Adicionado na posição 0 da lista...')
+print('-=') * 30
+
+print(f'Os valores digitados em ordem foram {lista}')
