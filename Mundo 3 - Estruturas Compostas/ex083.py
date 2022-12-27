d8ba1afe5c27c6abdb06101a87f6986376d0c10c
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2022-12-26 22:52:21 -0300

    início da resolução do ex083

--- Mundo 3 - Estruturas Compostas/ex083.py
+++ Mundo 3 - Estruturas Compostas/ex083.py
@@ -1,2 +1,11 @@
 # Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 # Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+
+expression = str(input('Digite uma expressão: '))
+parenteses_abertura = expression.count('(')
+parenteses_fechamento = expression.count(')')
+
+if parenteses_abertura == parenteses_fechamento:
+    print('Expressão válida.')
+else:
+    print('Expressão inválida.')
