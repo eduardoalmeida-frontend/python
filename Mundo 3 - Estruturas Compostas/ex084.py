d8dedc3a9767e20b2e5d337c0ffe2f1b25c8e8e1
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2022-12-27 22:22:54 -0300

    início da resolução do ex084

--- Mundo 3 - Estruturas Compostas/ex084.py
+++ Mundo 3 - Estruturas Compostas/ex084.py
@@ -3,3 +3,18 @@
 # a) quantas pessoas foram cadastradas;
 # b) uma listagem com as pessoas mais pesadas;
 # c) uma listagem com as pessoas mais leves.
+pessoas = list()
+pessoa = list()
+
+while True:
+    pessoa.append(str(input('Nome: ')))
+    pessoa.append(str(input('Peso: ')))
+    pessoas = pessoa[:]
+    resp = str(input('Quer continuar? [S/N] '))
+    while resp.upper() not in 'SN':
+        resp = str(input('Opção inválida. Quer continuar? [S/N] '))
+    if resp.upper() == 'N':
+        break
+
+# print(f'Ao todo, você cadastrou {len(pessoas) / 2} pessoas.')
+print(pessoas)
