a8f4754e087bf398ae7a02a545140c4176737bc5
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2022-12-27 22:28:41 -0300

    resolução do ex085

--- Mundo 3 - Estruturas Compostas/ex085.py
+++ Mundo 3 - Estruturas Compostas/ex085.py
@@ -1,3 +1,15 @@
 # Crie um programa onde o usuário possa digitar sete valores númericos.
 # Cadastre-os em uma lista única que mantenha separados os valores pares e ímpares.
 # No final, mostre os valores pares e ímpares em ordem crescente.
+numeros = [[], []]
+for i in range(0, 7):
+    num = int(input('Digite um valor: '))
+    if num % 2 == 0:
+        numeros[0].append(num) # pares
+    else:
+        numeros[1].append(num) # ímpares
+
+numeros[0].sort()
+print(f'Pares: {numeros[0]}')
+numeros[1].sort()
+print(f'Ímpares: {numeros[1]}')
