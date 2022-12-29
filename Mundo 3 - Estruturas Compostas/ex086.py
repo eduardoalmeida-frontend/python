b60800580f5966f00185c2f14802ee01c76348bf
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2022-12-28 21:48:05 -0300

    início da resolução do ex086

--- Mundo 3 - Estruturas Compostas/ex086.py
+++ Mundo 3 - Estruturas Compostas/ex086.py
@@ -1,2 +1,9 @@
 # Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado.
 # No final, mostre a matriz na tela, com a formatação correta.
+matriz = [[], [], [], [], [], [], [], [], []]
+for linha in range(0, 2):
+    for coluna in range(0, 2):
+        matriz[linha][coluna] = int(input('Digite um valor: '))
+
+for i in matriz:
+    print(f'[ {i} ]', end=' ')
